6a8c45cba5e9ee103eef53f58cf9160c73621ff2
s.sen <s.sen@cm.expert>
2020-05-24 00:00:05 +0300

    Wip implement loading store

--- examples/example_app/lib/main.dart
+++ examples/example_app/lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
+import 'state/loading/loading_store.dart';
+
 void main() {
   runApp(ExampleApp());
 }
 
 class ExampleApp extends StatelessWidget {
   ExampleApp({Key key}) : super(key: key);
+
+  LoadingStore store = LoadingStore();
 
   @override
   Widget build(BuildContext context) => MaterialApp(
@@ -14,6 +18,15 @@ class ExampleApp extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: const Placeholder(),
+        home: Column(
+          children: [
+            ValueListenableBuilder<bool>(
+              valueListenable: null,
+              builder: (context, value, child) {
+                return Placeholder();
+              },
+            )
+          ],
+        ),
       );
 }
